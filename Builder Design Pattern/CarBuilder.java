3f1c951524b3acfe26e3dd99eefdf471f1011218
DhruvilSoni <dhruvilsoni1703@gmail.com>
2021-05-31 18:48:31 +0530

    Update CarBuilder.java

--- Builder Design Pattern/CarBuilder.java
+++ Builder Design Pattern/CarBuilder.java
@@ -1,6 +1,6 @@
 package BuilderDesignPattern;
 
-public class Car {
+public class CarBuilder {
 
 	private String model;
 	private String color;
@@ -13,26 +13,50 @@ public class Car {
 	private boolean navigationSystem;
 	private boolean bluetooth;
 	
-	public Car(String model, String color, String fuelType, String engine, String breaks, 
-			int cost, int seats, double mileage_kmpl, boolean navigationSystem, boolean bluetooth)
-	{
+	public CarBuilder setModel(String model) {
 		this.model = model;
-		this.color = color;
-		this.fuelType = fuelType;
-		this.engine = engine;
-		this.breaks = breaks;
-		this.cost = cost;
-		this.seats = seats;
-		this.mileage_kmpl = mileage_kmpl;
-		this.navigationSystem = navigationSystem;
-		this.bluetooth = bluetooth;
+		return this;
 	}
-	
-	public String toString()
+	public CarBuilder setColor(String color) {
+		this.color = color;
+		return this;
+	}
+	public CarBuilder setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+		return this;
+	}
+	public CarBuilder setEngine(String engine) {
+		this.engine = engine;
+		return this;
+	}
+	public CarBuilder setBreaks(String breaks) {
+		this.breaks = breaks;
+		return this;
+	}
+	public CarBuilder setCost(int cost) {
+		this.cost = cost;
+		return this;
+	}
+	public CarBuilder setSeats(int seats) {
+		this.seats = seats;
+		return this;
+	}
+	public CarBuilder setMileage_kmpl(double mileage_kmpl) {
+		this.mileage_kmpl = mileage_kmpl;
+		return this;
+	}
+	public CarBuilder setNavigationSystem(boolean navigationSystem) {
+		this.navigationSystem = navigationSystem;
+		return this;
+	}
+	public CarBuilder setBluetooth(boolean bluetooth) {
+		this.bluetooth = bluetooth;
+		return this;
+	}
+	public Car getCar()
 	{
-		return "Car [Model="+model+", Color="+color+", Fuel Type="+fuelType+", Engine="+engine
-				+", Breaks="+breaks+", Cost="+cost+", Seats="+seats+", Mileage (kmpl)="+mileage_kmpl+
-				", Navigation System="+navigationSystem+", Bluetooth="+bluetooth+"]";
+		return new Car(model, color, fuelType, engine, breaks, cost, seats, mileage_kmpl, navigationSystem, 
+				bluetooth);
 	}
 	
 }
