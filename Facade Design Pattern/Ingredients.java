9d6068815778540a426e6c4a0f180bb5aebd574f
DhruvilSoni <dhruvilsoni1703@gmail.com>
2021-05-31 18:54:56 +0530

    Create Ingredients.java

---
+++ Facade Design Pattern/Ingredients.java
@@ -0,0 +1,14 @@
+package Dhruvil_Soni;
+
+public class Dhruvil_Soni_19BCP032_Ingredients {
+	
+	public String getPizzaItems()
+	{
+		return "Bread, Pizza, Sauce, Cheese";
+	}
+	public String getPastaItems()
+	{
+		return "Flour, Tomato paste, Onion";
+	}
+
+}
